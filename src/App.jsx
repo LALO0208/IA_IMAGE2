9669ae86f7ee93a4eb49b52b1914209bc838055e
Eduardo Narez <em5577595@gmail.com>
2023-05-24 20:34:59 -0600

    cambios app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-//import { Configuration, OpenAIApi } from 'openai'
-import { Configuration, OpenAIApi } from "../node_modules/openai/dist/api";
+import { Configuration, OpenAIApi } from 'openai'
 import './App.css'
 
 
