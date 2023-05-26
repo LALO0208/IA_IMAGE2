a575b35808ea00309337db2f5f074eb3662352c1
Eduardo Narez <em5577595@gmail.com>
2023-05-25 19:05:08 -0600

    cambios openIA

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { Configuration, OpenAIApi } from 'openai'
+import { Configuration, OpenAIApi } from '/vite-project2/node_modules/openai'
 import './App.css'
 
 
