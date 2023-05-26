e5da96a38e0f2314101bf530b7316c8efd8058a2
Eduardo Narez <em5577595@gmail.com>
2023-05-25 19:39:21 -0600

    cambios 3

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import openai from 'openai';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import openai from 'openai'; // Importar el módulo openai
+import App from './App.jsx';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <App openai={openai} /> {/* Pasar el módulo openai como una prop a App */}
+  </React.StrictMode>
+);
