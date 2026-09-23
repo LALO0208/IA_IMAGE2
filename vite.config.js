1772c8b8312ff7351cda15eaaad08ad8d660f162
Eduardo Narez <79491830+LALO0208@users.noreply.github.com>
2026-09-23 00:17:50 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/IA_IMAGE2/',
+ // base: '/IA_IMAGE2/',
   plugins: [react()],
   build: {
     rollupOptions: {
