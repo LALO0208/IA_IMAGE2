d5759fb939c2ff7740d583502053dc1639db7460
Eduardo Narez <79491830+LALO0208@users.noreply.github.com>
2026-09-22 22:23:11 -0600

    Actualizando ruta

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/IA_IMAGE2/',
   plugins: [react()],
   build: {
     rollupOptions: {
